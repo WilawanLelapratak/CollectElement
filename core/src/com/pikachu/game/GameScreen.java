d05cbace6fae5dfb1b3326854ed3e3607dae1269
WilawanLelapratak <wilawan.wove@gmail.com>
2017-01-04 06:30:51 +0700

    FinalGame II : Pikachu Run in Line

--- core/src/com/pikachu/game/GameScreen.java
+++ core/src/com/pikachu/game/GameScreen.java
@@ -7,17 +7,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class GameScreen extends ScreenAdapter {
 	private PiKaChuGame piKaChuGame;
 	private Texture piKaChuImg;
+	private int x;
+	private int y;
 	
 	public GameScreen (PiKaChuGame piKaChuGame) {
 		this.piKaChuGame = piKaChuGame;
 		piKaChuImg = new Texture("pikachu.png");
+		x = 100;
+		y = 100;
 	}
 	
 	@Override
     public void render(float delta) {
         SpriteBatch batch = piKaChuGame.batch;
+        x += 5;
         batch.begin();
-        batch.draw(piKaChuImg, 100, 100);
+        batch.draw(piKaChuImg, x, y);
         batch.end();
         
     }
