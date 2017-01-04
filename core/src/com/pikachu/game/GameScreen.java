26f3c06f4e0de4e507bc041457ce9cb3ca764558
WilawanLelapratak <wilawan.wove@gmail.com>
2017-01-04 07:20:16 +0700

    FinalGame VI : Add Background

--- core/src/com/pikachu/game/GameScreen.java
+++ core/src/com/pikachu/game/GameScreen.java
@@ -10,12 +10,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class GameScreen extends ScreenAdapter {
 	private PiKaChuGame piKaChuGame;
 	private Texture piKaChuImg;
+	private Texture bgImg;
 	private int x;
 	private int y;
 	
 	public GameScreen (PiKaChuGame piKaChuGame) {
 		this.piKaChuGame = piKaChuGame;
 		piKaChuImg = new Texture("pikachu.png");
+		bgImg = new Texture("background.png");
 		x = 100;
 		y = 100;
 	}
@@ -27,6 +29,7 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         update(delta);
         batch.begin();
+        batch.draw(bgImg, 0, 0);
         batch.draw(piKaChuImg, x, y);
         batch.end();
     }
